d600377970c882c69c7b8c995a99d7a3e7e6436b
crhuffer <crhuffer@gmail.com>
2018-11-03 11:49:26 -0700

    Feature added: save frames to disk
    Cleaned up code for saving screenshots to disk for later use.

--- SavingImages.py
+++ SavingImages.py
@@ -5,61 +5,33 @@ Created on Sat Nov  3 09:35:24 2018
 @author: crhuffer
 """
 
-from __future__ import division
-
 import time
-import os
-import os.path as osp
-import numpy as np
-import torch
 import cv2
-import pickle as pkl
-from utilis import load_classes, parse_cfg, get_test_input, create_module, letterbox_image, prep_image, filter_results
-import random
-from yolo_v3 import yolo_v3
-
-#import pandas as pd
 import datetime
 
+# %% Setup paths
 
-#layer_type_dic, module_list = create_module(blocks)
-# it might imporve performance
-#torch.backends.cudnn.benchmark = True
-
-# change cuda to True if you wish to use gpu
-cuda = False
 images = "../1RawData/"
 path_ProcessedData = "../2ProcessedData/"
-batch_size = 1
-confidence = float(0.7)
-nms_thesh = float(0.4)
-start = 0
-num_classes = 80
-cfg_path = "../4Others/yolov3.cfg"
-blocks = parse_cfg(cfg_path)
-model = yolo_v3(blocks)
-model.load_weights("../4Weights/yolov3.weights")
-colors = pkl.load(open("../4Others/pallete", "rb"))
-if cuda:
-    model = model.cuda()
-for params in model.parameters():
-    params.requires_grad = False
-model.layer_type_dic['net_info']["height"] = 320
-model.layer_type_dic['net_info']["width"] = 320
-inp_dim = int(model.layer_type_dic['net_info']["height"])
 
+# %% Setup input device
 
 DeviceID = 0
 cap = cv2.VideoCapture(DeviceID)
 assert cap.isOpened(), 'Cannot capture source'
 
+# %% Capture, display, and save frames from video source
 while cap.isOpened():
+    time.sleep(0.05)
     ret, frame = cap.read()
-    
-    if ret:   
-#        img = prep_image(frame, inp_dim)
-        filename = path_ProcessedData + 'D{}Date{}.png'.format(DeviceID, datetime.datetime.now().strftime('%Y%m%d%H%M%S%f'))
-#        print(filename)
+
+    if ret:
+
+        filename = (path_ProcessedData +
+                    'D{}Date{}.png'.format(DeviceID,
+                      datetime.datetime.now().strftime('%Y%m%d%H%M%S%f'))
+                    )
+        print(filename)
         cv2.imwrite(filename, frame)
-        
-    
+        cv2.imshow("frame", frame)
+        key = cv2.waitKey(1)
