f19c8e50988f3d30908a0809ccb8c3c9e3e295f8
crhuffer <crhuffer@gmail.com>
2018-11-11 13:01:45 -0500

    Update SavingImages.py
    Updated savingimages.py to capture two webcams.

--- SavingImages.py
+++ SavingImages.py
@@ -8,7 +8,7 @@ Created on Sat Nov  3 09:35:24 2018
 import time
 import cv2
 import datetime
-
+import numpy as np
 # %% Setup paths
 
 images = "../1RawData/"
@@ -16,22 +16,36 @@ path_ProcessedData = "../2ProcessedData/"
 
 # %% Setup input device
 
-DeviceID = 0
-cap = cv2.VideoCapture(DeviceID)
-assert cap.isOpened(), 'Cannot capture source'
+DeviceID1 = 0
+cap1 = cv2.VideoCapture(DeviceID1)
+assert cap1.isOpened(), 'Cannot capture source'
+
+DeviceID2 = 1
+cap2 = cv2.VideoCapture(DeviceID2)
+assert cap2.isOpened(), 'Cannot capture source'
 
 # %% Capture, display, and save frames from video source
-while cap.isOpened():
+while cap1.isOpened():
     time.sleep(0.05)
-    ret, frame = cap.read()
+    ret, frame1 = cap1.read()
+    ret, frame2 = cap2.read()
 
     if ret:
 
-        filename = (path_ProcessedData +
-                    'D{}Date{}.png'.format(DeviceID,
+        filename1 = (path_ProcessedData +
+                    'D{}Date{}.png'.format(DeviceID1,
                       datetime.datetime.now().strftime('%Y%m%d%H%M%S%f'))
                     )
-        print(filename)
-        cv2.imwrite(filename, frame)
-        cv2.imshow("frame", frame)
+        print(filename1)
+        cv2.imwrite(filename1, frame1)
+
+        filename2 = (path_ProcessedData +
+                    'D{}Date{}.png'.format(DeviceID2,
+                      datetime.datetime.now().strftime('%Y%m%d%H%M%S%f'))
+                    )
+        print(filename2)
+        cv2.imwrite(filename2, frame2)
+
+#        cv2.imshow("frame", np.concatenate((frame1, frame2), axis=1))
+        cv2.imshow('frame', frame1)
         key = cv2.waitKey(1)
