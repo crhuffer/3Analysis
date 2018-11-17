20eeea428cb35566e5dbced918c96dd51552eef3
crhuffer <crhuffer@gmail.com>
2018-11-17 16:35:24 -0500

    Update ExplorePredictions.py
    Added the datetime and detector of the image to the dataframe and the code required to append all of the predictions together for plotting and exploring purposes.

--- ExplorePredictions.py
+++ ExplorePredictions.py
@@ -8,6 +8,8 @@ Created on Sun Nov  4 15:00:36 2018
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import glob
+import datetime
 
 # %% Setup paths
 
@@ -15,11 +17,49 @@ path_RawData = "../1RawData/"
 path_ProcessedData = "../2ProcessedData/"
 path_Predictions = "../5Predictions/"
 
-filename_Predictions = path_Predictions + 'Predictions - Copy.csv'
+filename_Predictions = path_Predictions + 'Predictions - Copy.xlsx'
+
+version = 'V1'
+path_YOLOTaggedImagesCurrentVersion = path_ProcessedData + 'YOLOTagged' + version + '/'
 
 # %%
 
-df_Predictions = pd.read_csv(filename_Predictions, index_col=0)
+list_predictionfilenames = glob.glob(path_YOLOTaggedImagesCurrentVersion + '*.txt')
+
+# %%
+
+list_datetimes = []
+list_detector = []
+df_Predictions = pd.DataFrame()
+
+for filename in list_predictionfilenames:
+
+
+    # Get the values from the file and add them to what we have
+    df_Prediction = pd.read_csv(filename)
+
+    # Grab the datetime and detector number from the filename
+    current_datetime = datetime.datetime.strptime(filename[-24:-4], '%Y%m%d%H%M%S%f')
+    current_detector = int(filename[-29:-28])
+
+    # Add the current values for however many predictions are in this frame.
+    list_datetimes.extend([current_datetime]*len(df_Prediction))
+    list_detector.extend([current_detector]*len(df_Prediction))
+    ## TODO: Extract the datetime and detector from the filename, add as a new column.
+    if len(df_Predictions) == 0:
+        df_Predictions = df_Prediction.copy()
+    else:
+        df_Predictions = df_Predictions.append(df_Prediction, ignore_index=True)
+
+# add the detectors and datetimes to the dataframe
+df_Predictions['detector'] = list_detector
+df_Predictions['datetime'] = list_datetimes
+
+# %%
+
+df_Predictions.to_excel(filename_Predictions)
+
+#df_Predictions = pd.read_csv(filename_Predictions, index_col=0)
 
 # %%
 
