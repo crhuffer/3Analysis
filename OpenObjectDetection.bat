4b81138ae93ef128f6e549218023f9874484d6fa
crhuffer <crhuffer@gmail.com>
2018-11-18 10:06:06 -0500

    Create OpenObjectDetection.bat
    Added a bash script to setup the environment in anaconda prompt and move to the appropriate directory. Put a link to the script on the desktop for ease of use.

---
+++ OpenObjectDetection.bat
@@ -0,0 +1,9 @@
+:: activate the anaconda prompt 
+call "C:\ProgramData\Anaconda3\Scripts\activate.bat"
+:: move to the ObjectDetection folder
+cd ../Documents/GitHub/ObjectDetection/3Analysis
+:: run the following command in conda and /k leaves the window openfiles
+:: so that we can then choose what file we want to run.
+cmd /k conda activate ObjectDetection
+
+
